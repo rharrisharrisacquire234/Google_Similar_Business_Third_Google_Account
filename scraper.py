05c73e808f9bc18615adffdacbc8d74b8927813f
rharrisharrisacquire234 <Richard.harris@harrisacquire.com>
2026-02-13 17:41:39 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -97,7 +97,7 @@ def get_existing_websites(sheet):
 # SCRAPE
 # -------------------------------------------------
 async def scrape_city(page, city, business_query):
-    query = f"{business_query} {city}"
+    query = f"{business_query} {city} United Kingdom"
     print(f"\n🔍 {business_query} | 🌆 {city}", flush=True)
 
     await page.goto(
